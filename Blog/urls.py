cc61e78a339b4bf54095f9238617fbb11e33cf50
Florencia Cardozo <florencia.cardozo025@gmail.com>
2023-10-11 21:22:43 -0300

    Add files via upload
    Mi Segundo Commit

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from Blog import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    
+    path('admin/', admin.site.urls),
+    path('blog/', include('Blog.urls')),
 ]
